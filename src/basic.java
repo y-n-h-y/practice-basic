5f75f585509bdfc4b10d0fac89ca8028ba8286df
吉野弘之 <yoshinohiroyuki@yoshinohiroyuki-no-MacBook-Pro.local>
2022-02-06 17:52:52 +0900

    変数

--- src/basic.java
+++ src/basic.java
@@ -1,5 +1,25 @@
 public class basic {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, WEBCAMP!");
+        // System.out.println("Hello, Java!");
+
+        // int num1;
+        // String str1;
+
+        // num1 = 10;
+        // str1 = "Hello, World";
+
+        // System.out.println(num1);
+        // System.out.println(str1);
+
+        // num1 = 20;
+        // System.out.println(num1);
+
+        // int num2 = 30;
+
+        // System.out.println(num2);
+
+        final int MIN_NUMMBER = 10;
+        System.out.println(MIN_NUMMBER);
     }
 }
