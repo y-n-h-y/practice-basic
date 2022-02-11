15b738ae5f01609530776dbbdd9dae64800c262b
吉野弘之 <yoshinohiroyuki@yoshinohiroyuki-no-MacBook-Pro.local>
2022-02-11 16:18:23 +0900

    繰り返し文

--- src/basic.java
+++ src/basic.java
@@ -3,6 +3,8 @@ public class basic {
         // System.out.println("Hello, WEBCAMP!");
         // System.out.println("Hello, Java!");
 
+        //----------------------------------------
+
         // int num1;
         // String str1;
 
@@ -19,7 +21,107 @@ public class basic {
 
         // System.out.println(num2);
 
-        final int MIN_NUMMBER = 10;
-        System.out.println(MIN_NUMMBER);
-    }
+        //----------------------------------------
+
+        // final int MIN_NUMMBER = 10;
+        // System.out.println(MIN_NUMMBER);
+
+        //----------------------------------------
+
+        // String string1;
+
+        // string1 = "テスト";
+        // System.out.println(string1 + "の文字数:" + string1.length());
+
+        // string1 = "";
+        // System.out.println(string1 + "の文字数:" + string1.length());
+
+        // string1 = null;
+        // System.out.println(string1 + "の文字数:" + string1.length());
+        
+        //-----------------------------------------
+//    	final int LUCK_NUMMBER = 7;
+//    	String name = "hiro";
+//    	System.out.println(name);
+//    	name = "mac";
+//    	System.out.println(name);
+//    	
+//    	System.out.println(LUCK_NUMMBER);
+    	
+    	 //-----------------------------------------
+    	
+//    	long long1 = 100L;
+//    	System.out.println(long1);
+//    	
+//    	double double1 = 50.0;
+//    	System.out.println(double1);
+//    	
+//    	boolean boolean1 = true;
+//    	System.out.println(boolean1);
+    	
+    	//-----------------------------------------
+    	
+//    	int val = 2+5;
+//    	System.out.println(val);
+//    	val++;
+//    	System.out.println(val);
+//    	val *= 5;
+//    	System.out.println(val);
+//    	
+//    	boolean bool;
+//    	bool = (val == 50);
+//    	System.out.println(bool);
+//    	bool = (val < 50) && (val%10 == 0);
+//    	System.out.println(bool);
+    	
+    	//-----------------------------------------
+    	
+//    	int distance = 11;
+//    	if (distance <= 5) {
+//    		System.out.println("とても近いです");
+//    	} else if (distance >5 && distance <= 10) {
+//    		System.out.println("近いです");
+//    	} else if (distance >10 && distance <= 15) {
+//    		System.out.println("遠いです");
+//    	} else {
+//    		System.out.println("長いです");
+//    	}
+    	
+//    	String color = "a";
+//    	switch (color) {
+//    	case "red":
+//    		System.out.println("赤信号です");
+//    		break;
+//    	case "yellow":
+//    		System.out.println("黄信号です");
+//    		break;
+//    	case "blue":
+//    		System.out.println("青信号です");
+//    		break;
+//    	default:
+//    		System.out.println("あ");
+//    	}
+    	//-----------------------------------------
+    	
+//    	int nummber = 1;
+//    	while (nummber < 50) {
+//    		nummber *= 2;
+//    		System.out.println("basic" + nummber);
+//    	}
+    	
+    	//-----------
+//    	for (int nummber = 1; nummber <= 5; nummber++) {
+//    		System.out.println("basic =" + nummber);
+//    	}
+    	
+//    	int nummber = 1;
+//    	while (nummber < 5) {
+//    		System.out.println(nummber * nummber);
+//    		nummber++;	
+//    	}
+    	int array[] = {1,5,10,15};
+    	for (int i = 0; i < array.length; i++) {
+    		System.out.println(array[i]);
+    	}
+     }
 }
