a14cee29c123503308ff2d6ff9861a773a225a09
吉野弘之 <yoshinohiroyuki@yoshinohiroyuki-no-MacBook-Pro.local>
2022-02-24 22:29:58 +0900

    クラスメソッド

---
+++ src/src/Human01.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Human01 {
+	public String name;
+	public int age;
+	
+	
+public Human01() {
+	name = "山田";
+	age = 20;
+}
+
+public Human01 (String name, int age) {
+	this.name = name;
+	this.age = age;
+}
+
+}
